34198fc837cb18e58c40caee25b85be74fafb56c
Turchanov-Denis <96124363+Turchanov-Denis@users.noreply.github.com>
2023-02-19 20:36:38 +0200

    SFML testing setup[OK]

--- laboratory/lab1/source/main.cpp
+++ laboratory/lab1/source/main.cpp
@@ -1,9 +1,33 @@
-#include <iostream>
-#include "inc/AnimatedText.h"
+// #include <iostream>
+// #include "inc/AnimatedText.hpp"
+// int main()
+// {
+
+//     // std::string text = "awd";
+//     AnimatedText thing{"awd", 1000};
+//     thing.printAnimation();
+// }
+#include <SFML/Graphics.hpp>
+
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-    // std::string text = "awd";
-    AnimatedText thing{"awd", 10000};
-    thing.printAnimation();
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
