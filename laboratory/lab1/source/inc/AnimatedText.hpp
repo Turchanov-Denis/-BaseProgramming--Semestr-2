5cd27da08648350d31700d0b3329571561218f7b
Turchanov-Denis <96124363+Turchanov-Denis@users.noreply.github.com>
2023-02-20 22:20:23 +0200

    lab1: completed

--- laboratory/lab1/source/inc/AnimatedText.hpp
+++ laboratory/lab1/source/inc/AnimatedText.hpp
@@ -17,22 +17,44 @@ public:
         m_sleep = callback;
     }
     ~AnimatedText() {};
-    void setText(std::string str) {
+    /**
+     * setText setup text animation
+     *
+     * @param string:
+     */
+    void setText(const std::string& str) {
         m_text = str;
     }
+     /**
+     * setDuration setup delay animation
+     *
+     * @param delay: in ms
+     */
     void setDuration( int duration) {
         m_duration = duration;
     }
+    /**
+    * setSleepFunction setup metod of sleeping animation
+    *
+    * @param callback: is special sleep function
+    */
     void setSleepFunction(void (*callback)(int)) {
         m_sleep = callback;
     }
-    std::string  printAnimation()
+    std::string  printAnimation() 
     {
+        if (m_sleep == nullptr) {
+            throw std::runtime_error{ " Sleep function not exist && setup it " };
+        }
         if (m_is_animated_text.length() == m_text.length()) {
             return m_is_animated_text;
         }
-        m_is_animated_text += m_text[m_is_animated_text.length()];
+        changeTmpText(m_text[m_is_animated_text.length()]);
         m_sleep((m_duration));
         return m_is_animated_text;
+    }
+private:
+    void changeTmpText(char el) {
+        m_is_animated_text += el;
     }
 };
