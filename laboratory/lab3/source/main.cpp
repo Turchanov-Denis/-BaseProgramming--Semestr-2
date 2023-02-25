e6d3c451c4117a54abdae71bbc3594f8ee0f80ca
Turchanov-Denis <96124363+Turchanov-Denis@users.noreply.github.com>
2023-02-25 22:44:03 +0200

    lab3: [ tmp: to get the code ]

--- laboratory/lab3/source/main.cpp
+++ laboratory/lab3/source/main.cpp
@@ -7,15 +7,23 @@ private:
     double m_b;
 
 public:
-    Complex(double a = 0.0, double b = 0.0) : m_a(a), m_b(b) {}
-    Complex(const Complex& tmp){}
-    Complex&  operator=(Complex tmp)
+    explicit Complex(double a = 0.0, double b = 0.0) : m_a(a), m_b(b) {}
+    Complex(const Complex &tmp) {}
+    Complex& operator=(Complex tmp)
     {
         std::swap(m_a, tmp.m_a);
         std::swap(m_b, tmp.m_b);
     }
-    Complex& operator+=(const Complex& other){
-        
+    Complex& operator+=(const Complex &other)
+    {
+        this->m_a += other.m_a;
+        this->m_b += other.m_b;
+        return *this;
+    }
+    friend Complex operator+(Complex self, const Complex &other)
+    {
+        self += other;
+        return self;
     }
 };
 
