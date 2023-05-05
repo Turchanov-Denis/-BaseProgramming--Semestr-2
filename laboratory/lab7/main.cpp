488c5d3108e0f18327bbc0c639d1667aaa787285
Turchanov-Denis <turchanovdenis786@gmail.com>
2023-05-05 19:56:10 +0200

    lab 7[ complete without length, width, height as storage]

--- laboratory/lab7/main.cpp
+++ laboratory/lab7/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+
 enum class Color {
     Red,
     White,
@@ -26,12 +27,13 @@ class Figure {
 public:
     Color m_color;
     std::vector<Vertex> m_crd{};
-    double m_square;
-    Figure(std::initializer_list<double> list) {
+
+    Figure(std::initializer_list<double> list, Color color = Color::White) {
+        m_color = color;
         std::vector<double> tmp_crd{};
         for (double x: list)
             tmp_crd.push_back(x);
-        for (size_t i = 0; i < tmp_crd.size() / 2 + 2; i++) {
+        for (size_t i = 0; i <= tmp_crd.size() / 2 + 2; i++) {
             m_crd.push_back(Vertex(tmp_crd[i], tmp_crd[i + 1]));
             i++;
         }
@@ -42,28 +44,43 @@ public:
             }
         }
 //        std::cout << source_vertex.x <<"-"<< source_vertex.y << std::endl;
-        std::sort(m_crd.begin(),m_crd.end(), sort_by_angle);
+        std::sort(m_crd.begin(), m_crd.end(), sort_by_angle);
+    }
+
+    double getSquare() {
+
+        double res{};
+        for (size_t i = 0; i < m_crd.size() - 1; i++) {
+            //std::cout << i<< " - i - " << i+1 << std::endl;
+            res += m_crd[i].x * m_crd[i + 1].y;
+        }
+        for (size_t j = m_crd.size() - 1; j >= 1; j--) {
+            //std::cout << j<< " - j - " << j-1 << std::endl;
+            res -= m_crd[j].x * m_crd[j - 1].y;
+        }
+        res += m_crd[m_crd.size() - 1].x * m_crd[0].y;
+        res -= m_crd[0].x * m_crd[m_crd.size() - 1].y;
+        return 0.5 * res;
     }
 
     static bool sort_by_angle(Vertex arg1, Vertex arg2) {
+        //std::cout << source_vertex.x << " - - - "<< source_vertex.y << std::endl;
         Vertex
                 vector1 = {arg1.x - source_vertex.x, arg1.y - source_vertex.y},
                 vector2 = {arg2.x - source_vertex.x, arg2.y - source_vertex.y},
                 vector_OX = {0.0, 100.0};
         // find cos
-        float cos1 = (vector1.x * vector_OX.x + vector1.y * vector_OX.y) /
-                     ((pow(vector1.x * vector1.x + vector1.y * vector1.y, 0.5)) *
-                      (pow(vector_OX.x * vector_OX.x + vector_OX.y * vector_OX.y, 0.5)));
-        float cos2 = (vector_OX.x * vector2.x + vector_OX.y * vector2.y) /
-                     ((pow(vector_OX.x * vector_OX.x + vector_OX.y * vector_OX.y, 0.5)) *
-                      (pow(vector2.x * vector2.x + vector2.y * vector2.y, 0.5)));
+        double cos1 = (vector1.x * vector_OX.x + vector1.y * vector_OX.y) /
+                      ((pow(vector1.x * vector1.x + vector1.y * vector1.y, 0.5)) *
+                       (pow(vector_OX.x * vector_OX.x + vector_OX.y * vector_OX.y, 0.5)));
+        double cos2 = (vector_OX.x * vector2.x + vector_OX.y * vector2.y) /
+                      ((pow(vector_OX.x * vector_OX.x + vector_OX.y * vector_OX.y, 0.5)) *
+                       (pow(vector2.x * vector2.x + vector2.y * vector2.y, 0.5)));
 
         if (acos(cos1) > acos(cos2)) {
-            return true;
-        }
-        if (cos1 == cos2) {
             return false;
         }
+        return true;
     }
 
     void print() {
@@ -74,14 +91,30 @@ public:
 
 };
 
-class Triangle : private Figure {
+class Triangle : public Figure {
     // contained self methods
+    size_t limit = 3;
 public:
-    Triangle(std::initializer_list<double> list) : Figure(list) {};
+    Triangle(std::initializer_list<double> list) : Figure(list) {
+        if (m_crd.size() != limit) {
+            throw std::exception();
+        }
+    };
+};
+
+class Rectangle : public Figure {
+    // contained self methods
+    size_t limit = 4;
+public:
+    Rectangle(std::initializer_list<double> list) : Figure(list) {
+        if (m_crd.size() != limit) {
+            throw std::exception();
+        }
+    };
 };
 
 int main() {
-    Figure a({3.97, 0.82, 2.26, 3.69, 5.39, 2.16});
-    a.print();
+    Triangle a({3.97, 0.82, 2.26, 3.69, 5.39, 2.16});
+    std::cout << a.getSquare() << std::endl;
     return 0;
 }
