7546767e3ab2a5065b0a3b0b00bb136322057dd0
Turchanov-Denis <turchanovdenis786@gmail.com>
2023-05-12 19:10:14 +0200

    pr3 [complete]

--- practical/prc3/main.cpp
+++ practical/prc3/main.cpp
@@ -42,64 +42,74 @@ public:
         return std::vector<double>{angleA, angleB, angleC};
     }
 
+    Vertex getL1EndCor(){
+        const double PI = 3.14159265358979323846;
+        double alpha = (180 - getAngle()[0]* 180.f / PI - (180 - abs(getAngle()[1]*180.f / PI))) + getAngle()[2]* 180.f / PI;
+        double beta = 180 - abs(getAngle()[1]*180.f / PI);
+        double oppSide = 10.0*sin(alpha*3.14159/180);
+        double adjSide = 10.0*cos(alpha*3.14159/180);
+        std::cout << "alpha "<< alpha << std::endl;
+        std::cout << "beta "<< beta << std::endl;
+        std::cout << "coord L1 "<< oppSide << adjSide << std::endl;
+        return Vertex(oppSide,adjSide);
+    }
 
+    Vertex getSourceCord(){
+        return M2;
+    }
 };
 
 int main() {
-    const double PI = 3.14159265358979323846;
     KinematicSolver a(10.0,20.0,20.0,20.0);
-    double xDegrees = (180 - a.getAngle()[0]* 180.f / PI - (180 - abs(a.getAngle()[1]*180.f / PI))) + a.getAngle()[2]* 180.f / PI;
-    std::cout << xDegrees <<" - "<< 180 - abs(a.getAngle()[1]*180.f / PI) << std::endl;
-    std::cout << sin(xDegrees*3.14159/180) <<" "<< 10.0*sin(xDegrees*3.14159/180)<< std::endl;
-    std::cout << cos(xDegrees*3.14159/180) <<" "<< 10.0*cos(xDegrees*3.14159/180) << std::endl;
-//        sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-//        window.setFramerateLimit(60);
-//
-//        sf::VertexArray points(sf::Points, 3);
-//        points[0].position = sf::Vector2f(400, 300);
-//        points[1].position = sf::Vector2f(500, 200);
-//        points[2].position = sf::Vector2f(700, 150);
-//
-//        float pointRadius = 10.f;
-//
-//        sf::VertexArray lines(sf::LinesStrip, points.getVertexCount());
-//
-//        sf::VertexArray axis(sf::Lines, 4);
-//        axis[0].position = sf::Vector2f(0.f, 300.f);
-//        axis[1].position = sf::Vector2f(800.f, 300.f);
-//        axis[2].position = sf::Vector2f(400.f, 0.f);
-//        axis[3].position = sf::Vector2f(400.f, 600.f);
-//
-//        while (window.isOpen()) {
-//            sf::Event event;
-//            while (window.pollEvent(event)) {
-//                if (event.type == sf::Event::Closed)
-//                    window.close();
-//            }
-//
-//            window.clear();
-//
-//            // Отрисовка координатных осей
-//            window.draw(axis);
-//
-//            // Отрисовка соединительных линий между точками
-//            for (size_t i = 0; i < points.getVertexCount(); ++i) {
-//                lines[i].position = points[i].position;
-//                lines[i].color = sf::Color::White;
-//            }
-//            window.draw(lines);
-//
-//            // Отрисовка точек
-//            sf::CircleShape pointShape(pointRadius);
-//            pointShape.setFillColor(sf::Color::White);
-//
-//            for (size_t i = 0; i < points.getVertexCount(); ++i) {
-//                pointShape.setPosition(points[i].position);
-//                window.draw(pointShape);
-//            }
-//
-//            window.display();
-//        }
+    Vertex res = a.getL1EndCor();
+        sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+        window.setFramerateLimit(60);
+
+        sf::VertexArray points(sf::Points, 3);
+        points[0].position = sf::Vector2f(400, 300);
+        points[1].position = sf::Vector2f(400 + 10*res.x, 300-10*res.y);
+        points[2].position = sf::Vector2f(400 + 10*a.getSourceCord().x, 300- 10*a.getSourceCord().y);
+
+        float pointRadius = 10.f;
+
+        sf::VertexArray lines(sf::LinesStrip, points.getVertexCount());
+
+        sf::VertexArray axis(sf::Lines, 4);
+        axis[0].position = sf::Vector2f(0.f, 300.f);
+        axis[1].position = sf::Vector2f(800.f, 300.f);
+        axis[2].position = sf::Vector2f(400.f, 0.f);
+        axis[3].position = sf::Vector2f(400.f, 600.f);
+
+        while (window.isOpen()) {
+            sf::Event event;
+            while (window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+
+            window.clear();
+
+            // Отрисовка координатных осей
+            window.draw(axis);
+
+            // Отрисовка соединительных линий между точками
+            for (size_t i = 0; i < points.getVertexCount(); ++i) {
+                lines[i].position = points[i].position;
+                lines[i].color = sf::Color::White;
+            }
+            window.draw(lines);
+
+            // Отрисовка точек
+            sf::CircleShape pointShape(pointRadius);
+            pointShape.setFillColor(sf::Color::White);
+
+            for (size_t i = 0; i < points.getVertexCount(); ++i) {
+                pointShape.setPosition(points[i].position);
+                window.draw(pointShape);
+            }
+
+            window.display();
+        }
 
     return 0;
 }
